b98d1a9faef6279900240d135dffcb8338fe88ea
ccarvalho-jp <carvalhojp2410@gmail.com>
2026-02-06 00:32:55 -0300

    Add styles.js for React Native styling

---
+++ atividade-4/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+container: {
+ flex: 1,
+ justifyContent: 'center',
+ alignItems: 'center',
+ backgroundColor: 'blue',
+ },
+texto: {
+ fontSize: 25,
+ marginVertical: 10,
+ fontWeight: 'bold',
+ color: 'white',
+ },
+ textoNormal: {
+   fontSize: 25,
+   marginVertical: 8,
+  color: 'white',
+ }
+});
+export default styles;
