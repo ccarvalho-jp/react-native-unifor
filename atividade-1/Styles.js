cfb9e2f359609b64e5a910a2e27b8da08cc5e673
ccarvalho-jp <carvalhojp2410@gmail.com>
2026-02-06 00:29:38 -0300

    Add Styles.js

---
+++ atividade-1/Styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+container: {
+ flex: 1,
+ justifyContent: 'center',
+ alignItems: 'center',
+ },
+texto: {
+ fontSize: 25,
+ },
+});
+export default styles;
