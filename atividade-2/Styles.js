764db9fc362d8362f9e4101a02924dcf9a13540f
ccarvalho-jp <carvalhojp2410@gmail.com>
2026-02-06 00:31:07 -0300

    Create Styles.js

---
+++ atividade-2/Styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+container: {
+ flex: 1,
+ justifyContent: 'center',
+ alignItems: 'center',
+ },
+texto: {
+ fontSize: 25,
+ fontWeight: 'bold',
+ marginVertical: 10,
+ },
+ textoNormal: {
+   fontSize: 25,
+   marginVertical: 8,
+ }
+});
+export default styles;
